225191da106ce7afb1b38db31270361def75b0e5
Kjell Hedstrom <Kjell.Hedstrom@Gmail.com>
2018-05-17 22:56:20 -0600

    added SingleProducer-MultipleConsumer

--- test/test_mpsc_spmc_round_robin_queues.cpp
+++ test/test_mpsc_spmc_round_robin_queues.cpp
@@ -11,11 +11,12 @@
 #include <gtest/gtest.h>
 #include <string>
 #include "q/mpsc_receiver_round_robin.hpp"
+#include "q/spmc_sender_round_robin.hpp"
 #include "q/spsc_flexible_circular_fifo.hpp"
 #include "q/q_api.hpp"
 
 
-TEST(MPSC, CreateOneQueue) {
+TEST(MPSC_SPMC, CreateOneQueue_MPSC) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto queue = queue_api::CreateQueue<qtype>(10);
@@ -36,7 +37,7 @@ TEST(MPSC, CreateOneQueue) {
 }
 
 
-TEST(MPSC, CreateManyQueues) {
+TEST(MPSC_SPMC, CreateManyQueues) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    constexpr auto senderID = queue_api::index::sender;
@@ -57,7 +58,7 @@ TEST(MPSC, CreateManyQueues) {
    EXPECT_EQ(kSizeTotal, consumer.capacity_free());
 }
 
-TEST(MPSC, RoundRobinOfOne) {
+TEST(MPSC_SPMC, RoundRobinOfOne) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto queue = queue_api::CreateQueue<qtype>(2);
@@ -72,7 +73,7 @@ TEST(MPSC, RoundRobinOfOne) {
    EXPECT_EQ(0, current);
 }
 
-TEST(MPSC, RoundRobinOfMany) {
+TEST(MPSC_SPMC, RoundRobinOfMany) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto q1 = queue_api::CreateQueue<qtype>(2);
@@ -93,7 +94,7 @@ TEST(MPSC, RoundRobinOfMany) {
    EXPECT_EQ(1, current);
 }
 
-TEST(MPSC, full) {
+TEST(MPSC_SPMC, full) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto q1 = queue_api::CreateQueue<qtype>(1);
@@ -118,7 +119,7 @@ TEST(MPSC, full) {
    EXPECT_TRUE(consumer.full());
 }
 
-TEST(MPSC, size) {
+TEST(MPSC_SPMC, size) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto q1 = queue_api::CreateQueue<qtype>(1);
@@ -144,7 +145,7 @@ TEST(MPSC, size) {
 }
 
 
-TEST(MPSC, pop) {
+TEST(MPSC_SPMC, pop) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto q1 = queue_api::CreateQueue<qtype>(1);
@@ -180,7 +181,7 @@ TEST(MPSC, pop) {
 }
 
 
-TEST(MPSC, usage) {
+TEST(MPSC_SPMC, usage) {
    using element = std::string;
    using qtype = spsc::flexible::circular_fifo<element>;
    auto q1 = queue_api::CreateQueue<qtype>(1);
@@ -200,4 +201,85 @@ TEST(MPSC, usage) {
 
    s2.push(arg);
    EXPECT_EQ(100, consumer.usage());
+}
+
+
+
+TEST(MPSC_SPMC, CreateOneQueue_SPMC) {
+   using element = std::string;
+   using qtype = spsc::flexible::circular_fifo<element>;
+   auto queue = queue_api::CreateQueue<qtype>(10);
+   auto temporary = std::get<queue_api::index::sender>(queue);
+   auto consumer = std::get<queue_api::index::receiver>(queue);
+
+   // convert the setup to a MPSC setup
+   spmc::round_robin::Sender<qtype> producer({temporary});
+
+   std::string e;
+   EXPECT_TRUE(producer.empty());
+   EXPECT_FALSE(producer.full());
+   EXPECT_EQ(10, producer.capacity());
+   EXPECT_EQ(10, producer.capacity_free());
+   EXPECT_EQ(0, producer.size());
+   EXPECT_TRUE(producer.lock_free());
+   EXPECT_TRUE(producer.push(e));
+}
+
+
+TEST(MPSC_SPMC, CreateManyQueues_SPMC) {
+   using element = std::string;
+   using qtype = spsc::flexible::circular_fifo<element>;
+   constexpr auto senderID = queue_api::index::sender;
+   constexpr auto receiverID = queue_api::index::receiver;
+
+   constexpr size_t kSize = 10;
+   constexpr size_t kSizeTotal = kSize * kSize;
+   std::vector<queue_api::Sender<qtype>> senders;
+   std::vector<queue_api::Receiver<qtype>> receivers;
+   for ( size_t i = 0; i < 10; ++i) {
+      auto queue = queue_api::CreateQueue<qtype>(10);
+      senders.push_back(std::get<senderID>(queue));
+      receivers.push_back(std::get<receiverID>(queue));
+   }
+
+   spmc::round_robin::Sender<qtype> consumer({senders});
+   EXPECT_EQ(kSizeTotal, consumer.capacity());
+   EXPECT_EQ(kSizeTotal, consumer.capacity_free());
+}
+
+
+TEST(MPSC_SPMC, push_SPMC) {
+   using element = std::string;
+   using qtype = spsc::flexible::circular_fifo<element>;
+   auto q1 = queue_api::CreateQueue<qtype>(1);
+   auto q2 = queue_api::CreateQueue<qtype>(1);
+   auto r1 = std::get<queue_api::index::receiver>(q1);
+   auto s1 = std::get<queue_api::index::sender>(q1);
+   auto r2 = std::get<queue_api::index::receiver>(q2);
+   auto s2 = std::get<queue_api::index::sender>(q2);
+
+   // convert the setup to a MPSC setup
+   //
+   spmc::round_robin::Sender<qtype> producer({s1, s2});
+   std::string arg = "s0";
+   producer.push(arg);
+
+   std::string recv;
+   EXPECT_FALSE(r2.pop(recv));
+   EXPECT_TRUE(r1.pop(recv));
+   EXPECT_EQ("s0", recv);
+
+   arg = "s1";
+   producer.push(arg);
+   arg = "s2";
+   producer.push(arg);
+
+   EXPECT_TRUE(r2.pop(recv));
+   EXPECT_EQ("s1", recv);
+   EXPECT_TRUE(r1.pop(recv));
+   EXPECT_EQ("s2", recv);
+
+   EXPECT_TRUE(producer.push(arg));
+   EXPECT_TRUE(producer.push(arg));
+   EXPECT_FALSE(producer.push(arg));
 }
