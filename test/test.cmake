417dca0843d2c99901aed03de6b741a6a0a2dd0a
Kjell Hedström - seeking Senior Engineering roles as well as contract opportunities <kjell.hedstrom@gmail.com>
2023-12-11 19:11:25 -0700

    gitaction precursor, modernize use of gtest (#19)
    * added devcontainer
    * modernize use of gtest

---
+++ test/test.cmake
@@ -0,0 +1,34 @@
+# =====================================================
+#  Public Domain: Inspired by github.com/KjellKod/g3log/CMakeLists.cmake
+#  2015, April 30, @author Kjell.Hedstrom
+# =====================================================
+
+# ref: https://google.github.io/googletest/quickstart-cmake.html
+include(FetchContent)
+FetchContent_Declare(
+   googletest
+   URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+)
+
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+
+file(GLOB TEST_SRC_FILES "test/*.cpp")
+message("test files: ${TEST_SRC_FILES}")
+include_directories(${PROJECT_SRC})
+set(TestRunner UnitTestRunner)
+
+enable_testing()
+add_executable(UnitTestRunner 
+   ${TEST_SRC_FILES}
+)
+
+target_link_libraries(
+   UnitTestRunner 
+   ${Q_LIBRARY}
+   GTest::gtest_main
+)
+
+include(GoogleTest)
+gtest_discover_tests(UnitTestRunner)
