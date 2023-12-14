72ee97d16f652ddfba7651af26ea47f8a984b081
Kjell Hedstrom <Kjell.Hedstrom@Gmail.com>
2023-12-13 21:50:57 -0700

    ongoing restructure

---
+++ test/StopWatch.hpp
@@ -0,0 +1,41 @@
+/*
+* Not any company's property but Public-Domain
+* Do with source-code as you will. No requirement to keep this
+* header if need to use it/change it/ or do whatever with it
+*
+* Note that there is No guarantee that this code will work
+* and I take no responsibility for this code and any problems you
+* might get if using it.
+*
+* Originally published at https://github.com/KjellKod/Q
+*/
+
+#pragma once
+#include <chrono>
+
+   // inspired by github/KjellKod/StopWatch
+   struct StopWatch {
+      using seconds = std::chrono::seconds;
+      using milliseconds = std::chrono::milliseconds;
+      using nanoseconds = std::chrono::nanoseconds;
+      using clock = std::chrono::steady_clock;
+      clock::time_point start_;
+
+      StopWatch() :
+          start_(std::chrono::high_resolution_clock::now()) {}
+      ~StopWatch() = default;
+
+      auto now() const { 
+         return std::chrono::high_resolution_clock::now(); 
+      };
+
+      uint64_t ElapsedSec() const {
+         return std::chrono::duration_cast<seconds>(now() - start_).count();
+      }
+      uint64_t ElapsedMs() const {
+         return std::chrono::duration_cast<milliseconds>(now() - start_).count();
+      }
+      uint64_t ElapsedNs() const {
+         return std::chrono::duration_cast<nanoseconds>(now() - start_).count();
+      }
+   };
